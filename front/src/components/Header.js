acd04bdacc761457d7378e192e9e86e33c4ab027
zimablue14 <zimablue14@SungwooKims-MacBook-Air.local>
2022-10-12 12:04:50 +0900

    feat: FE/로그인 회원가입 임시 구현

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Nav, Modal, Form, Button } from "react-bootstrap";
+import { Nav, Modal, Form, Button, Col, Row } from "react-bootstrap";
 import { UserStateContext, DispatchContext } from "../App";
 
+import * as Api from "../apiMock";
 
 function Header() {
   const navigate = useNavigate();
@@ -15,6 +16,58 @@ function Header() {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  //useState로 email 상태를 생성함.
+  const [email, setEmail] = useState("");
+  //useState로 password 상태를 생성함.
+  const [password, setPassword] = useState("");
+
+  //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
+  const validateEmail = (email) => {
+    return email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  //위 validateEmail 함수를 통해 이메일 형태 적합 여부를 확인함.
+  const isEmailValid = validateEmail(email);
+  // 비밀번호가 4글자 이상인지 여부를 확인함.
+  const isPasswordValid = password.length >= 4;
+  //
+  // 이메일과 비밀번호 조건이 동시에 만족되는지 확인함.
+  const isFormValid = isEmailValid && isPasswordValid;
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // "user/login" 엔드포인트로 post요청함.
+      const res = await Api.post("user/login", {
+        email,
+        password,
+      });
+      // 유저 정보는 response의 data임.
+      const user = res.data;
+      // JWT 토큰은 유저 정보의 token임.
+      const jwtToken = user.token;
+      // sessionStorage에 "userToken"이라는 키로 JWT 토큰을 저장함.
+      sessionStorage.setItem("userToken", jwtToken);
+      // dispatch 함수를 이용해 로그인 성공 상태로 만듦.
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: user,
+      });
+
+      // 기본 페이지로 이동함.
+      navigate("/", { replace: true });
+    } catch (err) {
+      console.log("로그인에 실패하였습니다.\n", err);
+    }
+  };
+
 
   // 전역상태에서 user가 null이 아니라면 로그인 성공 상태임.
   const isLogin = !!userState.user;
@@ -65,34 +118,65 @@ function Header() {
             </Nav.Item>
 
             <Nav.Item>
-              <Nav.Link onClick={() => navigate("/register")}>회원가입</Nav.Link>
+              <Nav.Link onClick={() => navigate("/register")}>
+                회원가입
+              </Nav.Link>
             </Nav.Item>
           </>
         )}
       </Nav>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} >
         <Modal.Header closeButton>
           <Modal.Title>로그인</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>이메일</Form.Label>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="loginEmail">
+              <Form.Label>이메일 주소</Form.Label>
               <Form.Control
                 type="email"
-                placeholder="이메일을 입력해주세요."
-                autoFocus
+                autoComplete="on"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
+              {!isEmailValid && (
+                <Form.Text className="text-success">
+                  이메일 형식이 올바르지 않습니다.
+                </Form.Text>
+              )}
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group controlId="loginPassword" className="mt-3">
               <Form.Label>비밀번호</Form.Label>
               <Form.Control
                 type="password"
-                placeholder="비밀번호는 4글자 이상입니다."
+                autoComplete="on"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
+              {!isPasswordValid && (
+                <Form.Text className="text-success">
+                  비밀번호는 4글자 이상입니다.
+                </Form.Text>
+              )}
+            </Form.Group>
+
+            <Form.Group as={Row} className="mt-3 text-center">
+              <Col sm={{ span: 20 }}>
+                <Button variant="primary" type="submit" disabled={!isFormValid}>
+                  로그인
+                </Button>
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mt-3 text-center">
+              <Col sm={{ span: 20 }}>
+                <Button variant="light" onClick={() => navigate("/register")}>
+                  회원가입하기
+                </Button>
+              </Col>
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -101,7 +185,7 @@ function Header() {
           <Button variant="secondary" onClick={handleClose}>
             닫기
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" disabled={!isFormValid}>
             로그인
           </Button>
         </Modal.Footer>
