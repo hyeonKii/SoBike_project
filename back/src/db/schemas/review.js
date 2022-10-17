2fa1fbd1f201a5b8cf7b3f66eeb812dc3d460245
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-17 03:06:41 +0000

    FEAT/BE/review-email_성현

--- back/src/db/schemas/review.js
+++ back/src/db/schemas/review.js
@@ -10,6 +10,10 @@ const ReviewSchema = new Schema(
             type: String,
             required: true
         },
+        email: {
+            type: String,
+            required: true
+        },
         title: {
             type: String, 
             required: true, 
