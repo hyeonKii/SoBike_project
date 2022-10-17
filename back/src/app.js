0380fa5e832fd754f1a0c3a8c682441bafb4f61b
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-17 08:14:49 +0000

    Update app.js

--- back/src/app.js
+++ back/src/app.js
@@ -6,8 +6,8 @@ import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 import { userAuthRouter } from "./routes/userRouter";
 import { reviewRouter } from "./routes/reviewRouter";
-// import { bicycleLocationRouter } from "./routes/bicycleLocationRouter";
-// import { likeRouter } from "./routes/likeRouter";
+import { bicycleLocationRouter } from "./routes/bicycleLocationRouter";
+import { likeRouter } from "./routes/likeRouter";
 
 const app = express();
 
@@ -19,8 +19,8 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use("/users", userAuthRouter);
 app.use("/reviews", reviewRouter);
-// app.use("/datas", bicycleLocationRouter);
-// app.use("/datas/bicycle/location/likes", likeRouter);
+app.use("/datas", bicycleLocationRouter);
+app.use("/datas/bicycle/location/likes", likeRouter);
 
 app.get("/", (req, res) => {
     res.send("기본적인 페이지 접속을 하셨습니다. 파이팅!");
