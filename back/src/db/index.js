b154a6e5b42965fb049ea17d09203f20158df565
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-17 07:34:41 +0000

    bicycleLocation , Like 기능 구현

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -3,6 +3,8 @@ import { User } from "./models/User";
 import { UserImage } from "./models/UserImage";
 import { Review } from "./models/Review";
 import { Comment } from "./models/Comment";
+import { bicycleLocation } from "./models/bicycleLocation";
+import { Like } from "./models/Like";
 
 const DB_URL = process.env.MONGODB_URL || "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.js 파일을 확인해 주세요.";
 
@@ -20,5 +22,5 @@ db.on("error", (err) => {
     console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + err);
 });
 
-export { User, UserImage, Review, Comment };
+export { User, UserImage, Review, Comment, bicycleLocation, Like };
 
