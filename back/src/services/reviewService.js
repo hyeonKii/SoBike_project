570ca22a6e5e1327bc4329ca3e3256e9fe0b68a7
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-18 08:02:18 +0000

    사진 업로드 기능 구현

--- back/src/services/reviewService.js
+++ back/src/services/reviewService.js
@@ -1,89 +1,192 @@
-import { ReviewModel } from "../schemas/review";
-import { ReviewImage } from "./ReviewImage";
+import fs from "fs";
+import path from "path"
+import { Review, ReviewImage } from "../db";
 
-const Review =  {
-    create : async ({newReview}) => {
-        const createdNewReview = await ReviewModel.create(newReview);
-        return {reviewId: createdNewReview._id,
-            userId: createdNewReview.userId,
-            email: createdNewReview.email,
-            title: createdNewReview.title,
-            contents: createdNewReview.contents,
-            locationName: createdNewReview.locationName,
-            roadAddress: createdNewReview.roadAddress};
+const reviewService = {
+     addReview: async (userId, fields, files) => {
+        const {email, title, contents, locationName, roadAddress} = fields;
+        const newReview = {userId, email, title, contents, locationName, roadAddress}
+        // console.log(userId)
+        // console.log(fields)
+        // console.log(files)
+        const createdNewReview = await Review.create({newReview});
+
+        // console.log("created: ",createdNewReview.reviewId)
+
+        
+        const originalFilename = files.reviewFile.originalFilename;
+        const extension = path.extname(originalFilename);
+        let fileName;
+
+        if(originalFilename.split(".").length > 2) {
+            const name = originalFilename.split(".");
+
+            for(let i = 0; i < name.length-1; i++) {
+                fileName += name[i];
+            }
+        } else {
+            fileName = originalFilename.split(".")[0];
+        }
+
+        fileName = fileName + "-" + Date.now() + extension;
+
+        const oldPath = files.reviewFile.filepath;
+        const newPath = __dirname + "/../public/reviewImages/" + fileName;
+        // const currentReviewImageInfo = await ReviewImage.findById(reviewId);
+
+        const reviewId = createdNewReview.reviewId
+        console.log("reviewId: ", reviewId)
+        console.log("fileName: ", fileName)
+        const createdReviewImage = await ReviewImage.create(reviewId, fileName);
+        if(!createdReviewImage) throw new Error("DB에 이미지 생성 실패");
+
+        fs.rename(oldPath, newPath, async (err) => {
+            if(err) throw new Error("이미지 업로드 실패");
+        });
+
+        createdNewReview.image = createdReviewImage.image
+        createdNewReview.errorMessage = null;
+
+        console.log("location: ",createdNewReview)
+        return createdNewReview;
     },
 
-    findById: async ({reviewId}) => {
-        // console.log(reviewId)
-        console.log("a")
-        const review = await ReviewModel.findOne({_id: reviewId});
-        const reviewImage = await ReviewImage.findById(reviewId)
-
-        return {reviewId: review._id,
-            userId: review.userId,
-            email: review.email,
-            title: review.title,
-            contents: review.contents,
-            locationName: review.locationName,
-            roadAddress: review.roadAddress,
-            reviewImageId: reviewImage.reviewImageId,
-            reviewImage: reviewImage.reviewImage }
+    getReviews: async ()=> {
+        // console.log("1sdfasdf")
+        const reviews = await Review.findAll();
+        console.log("service:" ,reviews)
+        return reviews;
     },
+    // getReviewImage: async ()=> {
+    //     // console.log("1sdfasdf")
+    //     const reviewImage = await ReviewImage.findAll();
+    //     console.log("service:" ,reviewImage)
+    //     return reviewImage;
+    // },
 
-    update: async ({reviewId, fieldToUpdate, newValue})=> {
+    getReview: async ({reviewId})=>{
         // console.log("reviewId: ", reviewId)
-        const filter = {_id: reviewId};
-        const update = {[fieldToUpdate]: newValue};
-        const option = {returnOriginal : false};
-        // console.log("{reveiwId, fieldToUpdate, newValue}: ", {reviewId, fieldToUpdate, newValue})
-        const updatedReview = await ReviewModel.findOneAndUpdate(
-            filter,
-            update,
-            option
-        );
-        // console.log(updatedReview)
-        return {reviewId: updatedReview._id,
-            userId: updatedReview.userId,
-            email: updatedReview.email,
-            title: updatedReview.title,
-            contents: updatedReview.contents,
-            locationName: updatedReview.locationName,
-            roadAddress: updatedReview.roadAddress };
+        const review = await Review.findById({reviewId});
+        // console.log(review)
+
+        return review;
+    },
+    
+    setReview: async (reviewId, fields, files)=>{
+        let review = null;
+        const {title, contents, locationName, roadAddress} = fields
+        // console.log("reviewId", reviewId)
+        // console.log("reviewService: ", reviewId )
+        // console.log("reviewService: ", fields )
+        // console.log("reviewService: ", files )
+
+        if (title) {
+            const fieldToUpdate = "title";
+            const newValue = title;
+            // console.log(newValue)
+            review = await Review.update({ reviewId, fieldToUpdate, newValue });
+            console.log(review)
+         }
+
+        if (contents) {
+            const fieldToUpdate = "contents";
+            const newValue = contents;
+            // console.log(newValue)
+            review = await Review.update({ reviewId, fieldToUpdate, newValue });
+        }
+
+        if (locationName) {
+            const fieldToUpdate = "locationName";
+            const newValue = locationName;
+            // console.log(newValue)
+            review = await Review.update({ reviewId, fieldToUpdate, newValue });
+        }
+
+        if (roadAddress) {
+            const fieldToUpdate = "roadAddress";
+            const newValue = roadAddress;
+            // console.log(newValue)
+            review = await Review.update({ reviewId, fieldToUpdate, newValue });
+        }
+
+        const originalFilename = files.reviewFile.originalFilename;
+        const extension = path.extname(originalFilename);
+        let fileName;
+
+        if(originalFilename.split(".").length > 2) {
+            const name = originalFilename.split(".");
+
+            for(let i = 0; i < name.length-1; i++) {
+                fileName += name[i];
+            }
+        } else {
+            fileName = originalFilename.split(".")[0];
+        }
+
+        fileName = fileName + "-" + Date.now() + extension;
+
+        const oldPath = files.reviewFile.filepath;
+        const newPath = __dirname + "/../public/reviewImages/" + fileName;
+        const currentReviewImageInfo = await ReviewImage.findById(reviewId);
+        
+        if(!currentReviewImageInfo) {
+            // DB에 저장된 이미지가 없으면 생성
+            const createReviewImage = await ReviewImage.create(reviewId, fileName);
+
+            if(!createReviewImage) throw new Error("DB에 이미지 생성 실패");
+
+            fs.rename(oldPath, newPath, async (err) => {
+                if(err) throw new Error("이미지 업로드 실패");
+            });
+
+            review.image = createReviewImage.image
+        } else {
+            // DB에 이미가 있으면 업데이트
+            const fieldToUpdate = "image";
+            const newValue = fileName;
+            const updatedReviewImage = await ReviewImage.update(reviewId, fieldToUpdate, newValue);
+            
+            review.image = updatedReviewImage.image;
+
+            fs.unlink(`src/public/images/${currentReviewImageInfo.image}`, (err) => {
+                // if(err) throw new Error("이미지 삭제 실패");
+                console.log("이미지 삭제 실패")
+            })
+
+            fs.rename(oldPath, newPath, (err) => {
+                if(err) throw new Error("이미지 업로드 실패");
+            });  
+        }
+
+        review.errorMessage = null;
+
+
+
+        return review;
     },
 
-    findAll: async ()=> {
-        console.log("afdf")
-        const reviews = await ReviewModel.find();
-     
-        const getReviewImage = await ReviewImage.findAll();
-        // console.log("getReviewImage: ",getReviewImage) 
-        const reviewList = reviews.map( (review)=>{
-            const reviewId = review._id;
-            // console.log("review:", review)
-            // console.log("reviewId: ", reviewId)
-            const reviewImage = getReviewImage.filter(image => image.reviewId === reviewId)
-            // console.log("reviewImage: ", reviewImage)
-            // console.log("reviewImageId: ", reviewImage[0].reviewImageId)
-            return {reviewId: review._id,
-                userId: review.userId,
-                email: review.email,
-                title: review.title,
-                contents: review.contents,
-                locationName: review.locationName,
-                roadAddress: review.roadAddress,
-                reviewImageId: reviewImage[0].reviewImageId,
-                reviewImage: reviewImage[0].reviewImage }
-        })
-        return reviewList;
-    },
+    delReview: async ({ reviewId }) => {
+        const deletedReview = await Review.delete({ reviewId });
+        
+        if(deletedReview) {
+            const deletedReviewImage = await ReviewImage.delete(reviewId);
+            
+            if(deletedReviewImage) {
+                fs.unlink(`src/public/images/${deletedReviewImage.image}`, (err) => {
+                    // if(err) throw new Error("이미지 삭제 실패");
+                    console.log("이미지 삭제 실패")
+                });
+            } else {
+                return deletedReviewImage;
+            }
+        } else {
+            return deletedReview;
+        }
+        
+        deletedReview.errorMessage = null;
 
-    delete: async ({ reviewId })=> {
-        const filter = { _id: reviewId };
-        console.log(reviewId)
-        const deleteReview = await ReviewModel.findOneAndDelete(filter);
-        return deleteReview;
-      }
-
+        return deletedReview;
+    }
 };
 
-export { Review };
+export { reviewService };
