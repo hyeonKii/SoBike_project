78a63ccc495f8f2365ebcdf1059ca1105d8af6d4
zimablue <working.zima@gmail.com>
2022-10-19 12:39:02 +0900

    메인 잠시 중단

--- front/src/components/pages/Sections.js
+++ front/src/components/pages/Sections.js
@@ -13,12 +13,11 @@ const Sections = () => {
         <div className="wrapper1">
           <div className="text-wrapper1">
             <div className="title1">
-              <span>탄소가 환경에 미치는 영향</span>
+              <span>남들보다 더 상쾌한 아침</span>
             </div>
             <div className="content1">
               <p>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                amet
+                서울시에는 5754개의 자전거 대여소에 <br/>40,500대의 자전거가 있다는 것을 아시나요?
               </p>
               <p>
                 sint. Velit officia consequat duis enim velit mollit.
