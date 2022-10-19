d2aa84a7cb089130b710c93853ddc0d45ae75c43
zimablue <working.zima@gmail.com>
2022-10-19 14:08:04 +0900

    잠시 멈춤2

--- front/src/components/pages/Sections.js
+++ front/src/components/pages/Sections.js
@@ -13,17 +13,16 @@ const Sections = () => {
         <div className="wrapper1">
           <div className="text-wrapper1">
             <div className="title1">
-              <span>남들보다 더 상쾌한 아침</span>
+              <span>가장 가까운 대중교통 정류장</span>
             </div>
             <div className="content1">
               <p>
-                서울시에는 5754개의 자전거 대여소에 <br/>40,500대의 자전거가 있다는 것을 아시나요?
+                서울시에 5754개의 자전거 대여소에 <br/>40,500대의 공공 자전거가 있다는 것을 아시나요?<br/>
+                이는 평균 240~350m 마다 <br/>자전거를 대여할 수 있고 반납할 수 있다는 말입니다.
               </p>
               <p>
-                sint. Velit officia consequat duis enim velit mollit.
-                Exercitation
+                
               </p>
-              <p>veniam consequat sunt nostrud amet.</p>
             </div>
           </div>
           <div className="chart"></div>
@@ -33,7 +32,7 @@ const Sections = () => {
           <div className="chart">chart1</div>
           <div className="text-wrapper1">
             <div className="title1">
-              <span>운송수단이 개인 탄소배출량에 어쩌구</span>
+              <span>남들보다 더 상쾌한 아침</span>
             </div>
             <div className="content1">
               <p>
