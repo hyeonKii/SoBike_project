1a28a88b876e777cb3039cbe00116ff21278f0fa
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-20 04:18:07 +0000

    Update Review.js

--- back/src/db/models/Review.js
+++ back/src/db/models/Review.js
@@ -15,12 +15,10 @@ const Review =  {
     },
 
     findById: async ({reviewId}) => {
-        // console.log(reviewId)
-        // console.log("a")
         const review = await ReviewModel.findOne({_id: reviewId});
-        // console.log("findById: ", review)
+
         const reviewImage = await ReviewImage.findById(reviewId)
-        // console.log("reviewImage: ", reviewImage)
+ 
         if(!reviewImage){
             console.log("!23344")
             const reviewData= {reviewId: reviewId,
@@ -30,7 +28,7 @@ const Review =  {
             contents: review.contents,
             locationName: review.locationName,
             roadAddress: review.roadAddress}
-            console.log("ReviewData: ", reviewData)
+
             return reviewData
         }else { const reviewData =  {reviewId: reviewId,
             userId: review.userId,
@@ -41,22 +39,21 @@ const Review =  {
             roadAddress: review.roadAddress,
             reviewImageId: reviewImage.reviewImageId,
             reviewImage: reviewImage.reviewImage }
-        console.log("reviewData: ", reviewData)
         return reviewData}                   
     },
 
     update: async ({reviewId, fieldToUpdate, newValue})=> {
-        // console.log("reviewId: ", reviewId)
+
         const filter = {_id: reviewId};
         const update = {[fieldToUpdate]: newValue};
         const option = {returnOriginal : false};
-        // console.log("{reveiwId, fieldToUpdate, newValue}: ", {reviewId, fieldToUpdate, newValue})
+  
         const updatedReview = await ReviewModel.findOneAndUpdate(
             filter,
             update,
             option
         );
-        // console.log(updatedReview)
+
         return {reviewId: updatedReview._id,
             userId: updatedReview.userId,
             email: updatedReview.email,
@@ -71,24 +68,13 @@ const Review =  {
         const reviews = await ReviewModel.find();
      
         const getReviewImage = await ReviewImage.findAll();
-        // console.log("getReviewImage: ",getReviewImage) 
+     
         const reviewList = reviews.map( (review)=>{
             const reviewId = review._id;
-            // console.log("review:", review)
-            // console.log("reviewId: ", reviewId)
+           
             const reviewImage = getReviewImage.filter(image => image.reviewId === reviewId)
             console.log("reviewImage: ", reviewImage)
-            // console.log("reviewImageId: ", reviewImage[0].reviewImageId)
-            // console.log("findAll: ",{reviewId: review._id,
-            //     userId: review.userId,
-            //     email: review.email,
-            //     title: review.title,
-            //     contents: review.contents,
-            //     locationName: review.locationName,
-            //     roadAddress: review.roadAddress,
-            //     reviewImageId: reviewImage[0].reviewImageId,
-            //     reviewImage: reviewImage[0].reviewImage 
-            // })
+         
             if(reviewImage.length ===0){return {reviewId: review._id,
                 userId: review.userId,
                 email: review.email,
@@ -114,7 +100,9 @@ const Review =  {
         const filter = { _id: reviewId };
         console.log(reviewId)
         const deleteReview = await ReviewModel.findOneAndDelete(filter);
-        return deleteReview;
+        const deletingImage = await ReviewImage.findById(reviewId)
+        console.log("deletingImage: ", deletingImage)
+        return {deleteReview, deletingImage};
       }
 
 };
