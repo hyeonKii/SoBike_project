dff6f87d431a822182b5de3d9d2aecccfd01d4e0
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-20 04:19:46 +0000

    Update bicycleLocationRouter.js

--- back/src/routes/bicycleLocationRouter.js
+++ back/src/routes/bicycleLocationRouter.js
@@ -1,7 +1,5 @@
 import {Router} from "express";
 import { loginRequired } from "../middlewares/loginRequired";
-// const reviewRouter = require("express").Router();
-// const { login_required } = require( "../middlewares/login_required");
 import {bicycleLocationService} from "../services/bicycleLocationService";
 
 const bicycleLocationRouter = Router();
@@ -23,8 +21,7 @@ bicycleLocationRouter.get("/bicycle/location", async (req, res, next)=> {
 });
 bicycleLocationRouter.get("/bicycle/locationName", async (req, res, next)=> {
     try{
-        // const longitude = req.body.longitude;
-        // const latitude = req.body.latitude;
+
         const locationName = req.body.locationName
 
         console.log(locationName)
@@ -36,8 +33,6 @@ bicycleLocationRouter.get("/bicycle/locationName", async (req, res, next)=> {
 });
 bicycleLocationRouter.get("/bicycleAddress", async (req, res, next)=> {
     try{
-        // const longitude = req.body.longitude;
-        // const latitude = req.body.latitude;
         const locationName = req.body.locationName
 
         console.log(locationName)
@@ -49,11 +44,6 @@ bicycleLocationRouter.get("/bicycleAddress", async (req, res, next)=> {
 });
 bicycleLocationRouter.get("/bicycle/locations", async (req, res, next)=> {
     try{
-        // const longitude = req.body.longitude;
-        // const latitude = req.body.latitude;
-        // const locationName = req.body.locationName
-
-        // console.log(locationName)
         const location = await bicycleLocationService.getLocations()
         res.status(200).send(location);
     }catch(error){
@@ -63,63 +53,18 @@ bicycleLocationRouter.get("/bicycle/locations", async (req, res, next)=> {
 bicycleLocationRouter.get("/bicycle/locationsByCurrentLocation?:userId?:latitude?:longitude", async (req, res, next)=> {
     try{
         console.log("asfdsf")
-        // const userId = req.currentUserId;
         const userId = req.query.userId
         console.log("userId: ",userId)
-        // console.log("req: ", req)
         const longitude = parseFloat(req.query.longitude);
         const latitude = parseFloat(req.query.latitude);
-        // const locationName = req.body.locationName
         console.log("longitude: ", longitude)
         console.log("latitude: ", latitude)
-        // console.log(locationName)
         const location = await bicycleLocationService.getLocationsByCurrentLocations({userId, longitude, latitude})
         res.status(200).send(location);
     }catch(error){
         next(error);
     }
 });
-
-// bicycleLocationRouter.put("/datas/bicycle/location", async (req, res, next)=> {
-//     try {
-//         const userId = req.params.userId;
-//         const locationId = req.body.locationId;
-//         const isLike = req.body.isLike;
-
-//         // console.log("isLike: ",isLike)
-//         const toUpdate = {isLike};
-//         // console.log(toUpdate)
-//         const updatedLike = await bicycleLocationService .setLike({userId, locationId, toUpdate});
-
-//         if (updatedLike.errorMessage){
-//             throw new Error(updatedLike.errorMessage);
-//         }
-
-//         res.status(200).json(updatedLike);
-//     }catch(err){
-//         next(err);
-//     }
-// });
-
-// bicycleLocationRouter.delete("/datas/bicycle/location/likes/:userId/",  async(req, res, next) => {
-//     try{
-//         const userId = req.params.userId;
-//         const locationId = req.body.locationId;
-//         console.log("userId:",userId)
-//         console.log("locationId:",locationId)
-//         // console.log("reviewId: ", reviewId)
-//         const deletedLike = await bicycleLocationService .delLike({userId, locationId});
-//         // console.log(deletedReview)
-//         if(deletedLike.errorMessage){
-//             throw new Error(deletedLike.errorMessage);
-//         }
-//         res.status(200).json(deletedLike);
-//     }catch(error){
-//         next(error);
-//     }
-// // }
-// );
-
 
 
 
