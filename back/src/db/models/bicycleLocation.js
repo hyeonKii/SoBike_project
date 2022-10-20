5ea0396be5e93a6072c2f6a2ef752ccba6537b8b
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-20 13:52:16 +0000

    Update bicycleLocation.js

--- back/src/db/models/bicycleLocation.js
+++ back/src/db/models/bicycleLocation.js
@@ -4,24 +4,14 @@ import {Like} from "./Like"
 const bicycleLocation =  {
     findByLocation: async ({longitude, latitude}) => {
 
-        console.log("longitude: ", longitude)
-        console.log("latitude: ", latitude)
-        console.log("longitude+0.5: ", longitude+0.5)
-        console.log("latitude+0.5: ", latitude+0.5)
-
         const bicycleLocation = await bicycleLocationModel.find( {longitude:longitude, latitude:latitude});
        
-        console.log("longitude: ", longitude)
-        console.log("latitude: ", latitude)
-        console.log("bicycleLocation: ", bicycleLocation)
         return bicycleLocation},
 
     findByLocationName: async ({locationName}) => {
 
-        console.log(locationName)
-
         const bicycleLocation = await bicycleLocationModel.find({locationName: {$regex: locationName}});
-        console.log("after: ", bicycleLocation)
+
         return bicycleLocation},
 
     findByLocationId: async (locationId) => {
@@ -36,32 +26,7 @@ const bicycleLocation =  {
                 roadAdress: bicycleLocation[0].roadAddress
                 }},
 
-    // findByCurrentLocations: async ({userId, longitude, latitude}) => {
 
-    //     console.log("a")
-    //     const bicycleLocation = await bicycleLocationModel.find({"longitude":{$gt:longitude-0.0049, $lt:longitude+0.0049}, "latitude":{$gt: latitude-0.0049, $lt:latitude +0.0049}});
-
-    //     const likedLocation = [];
-    //     const locationInfo = [];
-    //     for (let i = 0;i<bicycleLocation.length;i++){
-    //         const locationId = bicycleLocation[i].rentalLocationId
-    //         likedLocation[i] = await Like.findByUser(userId, locationId)
-    //         console.log("likedLocation[i]: ", likedLocation[i])
-    //         console.log("bicycledata: ", bicycleLocation[i])
-    //         locationInfo[i] = {
-    //             rentalLocationId: bicycleLocation[i].rentalLocationId,
-    //             roadAddress: bicycleLocation[i].roadAddress,
-    //             locationName: bicycleLocation[i].locationName,
-    //             latitude: bicycleLocation[i].latitude,
-    //             longitude: bicycleLocation[i].longitude,
-    //             userId: userId, 
-    //             liked: likedLocation[i].liked
-    //         }
-    //         console.log("locaitonInfo[i]: ", locationInfo[i])
-    //             }
-
-
-    //     return locationInfo},
     findByCurrentLocations: async ({userId, longitude, latitude}) => {
         let bicycleLocation = await bicycleLocationModel.find({"longitude":{$gt:longitude-0.0049, $lt:longitude+0.0049}, "latitude":{$gt: latitude-0.0049, $lt:latitude +0.0049}});
 
@@ -78,7 +43,6 @@ const bicycleLocation =  {
                 bicycleLocation[i]._doc.isLike = false;
             }
         }
-        // console.log("bicycleLocation: " + bicycleLocation)
         return bicycleLocation
     },
 
@@ -93,7 +57,6 @@ const bicycleLocation =  {
         return location;
     },
     findAll: async ()=> {
-        console.log("afdf")
         const bicycleLocation = await bicycleLocationModel.find();
 
         return bicycleLocation;
