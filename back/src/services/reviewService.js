44ad4b6416f2f9e97cbea5dbb67b1e36b4b659ac
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-21 10:58:20 +0000

    public -> uploads 수정

--- back/src/services/reviewService.js
+++ back/src/services/reviewService.js
@@ -30,13 +30,13 @@ const reviewService = {
             fileName = fileName + "-" + Date.now() + extension;
     
             const oldPath = files.reviewFile.filepath;
-            const newPath = __dirname + "/../public/reviewImages/" + fileName;
+            const newPath = __dirname + "/../uploads/reviewImages/" + fileName;
        
     
             const reviewId = createdNewReview.reviewId
             console.log("reviewId: ", reviewId)
             console.log("fileName: ", fileName)
-            const createdReviewImage = await ReviewImage.create(reviewId, "/public/reviewImages/" + fileName);
+            const createdReviewImage = await ReviewImage.create(reviewId, "/uploads/reviewImages/" + fileName);
             console.log("createdReviewImage: ", createdReviewImage)
             if(!createdReviewImage) throw new Error("DB에 이미지 생성 실패");
     
@@ -119,12 +119,12 @@ const reviewService = {
             fileName = fileName + "-" + Date.now() + extension;
     
             const oldPath = files.reviewFile.filepath;
-            const newPath = __dirname + "/../public/reviewImages/" + fileName;
+            const newPath = __dirname + "/../uploads/reviewImages/" + fileName;
             const currentReviewImageInfo = await ReviewImage.findById(reviewId);
             
             if(!currentReviewImageInfo) {
                 // DB에 저장된 이미지가 없으면 생성
-                const createReviewImage = await ReviewImage.create(reviewId, "/public/reviewImages/" + fileName);
+                const createReviewImage = await ReviewImage.create(reviewId, "/uploads/reviewImages/" + fileName);
     
                 if(!createReviewImage) throw new Error("DB에 이미지 생성 실패");
     
@@ -136,12 +136,12 @@ const reviewService = {
             } else {
                 // DB에 이미가 있으면 업데이트
                 const fieldToUpdate = "image";
-                const newValue = "/public/reviewImages/" + fileName;
+                const newValue = "/uploads/reviewImages/" + fileName;
                 const updatedReviewImage = await ReviewImage.update(reviewId, fieldToUpdate, newValue);
                 
                 review.reviewImage = updatedReviewImage.image;
                 // console.log("updatedReview: ",review)
-                fs.unlink(`src/public/reviewImages/${currentReviewImageInfo.image}`, (err) => {
+                fs.unlink(`src/uploads/reviewImages/${currentReviewImageInfo.image}`, (err) => {
                     // if(err) throw new Error("이미지 삭제 실패");
                     console.log("이미지 삭제 실패")
                 })
@@ -165,7 +165,7 @@ const reviewService = {
             const deletedReviewImage = await ReviewImage.delete(reviewId);
             
             if(deletedReviewImage) {
-                fs.unlink(`src/public/images/${deletedReviewImage.image}`, (err) => {
+                fs.unlink(`src/uploads/images/${deletedReviewImage.image}`, (err) => {
                     // if(err) throw new Error("이미지 삭제 실패");
                     console.log("이미지 삭제 실패")
                 });
