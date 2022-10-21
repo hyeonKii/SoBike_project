e5d23e134fda12a52151268174b09f5394b35cbf
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-21 13:22:46 +0000

    Update reviewService.js

--- back/src/services/reviewService.js
+++ back/src/services/reviewService.js
@@ -6,12 +6,8 @@ const reviewService = {
      addReview: async (userId, fields, files) => {
         const {email, title, contents, locationName, roadAddress} = fields;
         const newReview = {userId, email, title, contents, locationName, roadAddress}
-        console.log(userId)
-        console.log(fields)
- 
+
         const createdNewReview = await Review.create(newReview);
-        console.log("여기2")
-        console.log(createdNewReview)
        
         if(files.reviewFile) {const originalFilename = files.reviewFile.originalFilename;
             const extension = path.extname(originalFilename);
@@ -34,10 +30,9 @@ const reviewService = {
        
     
             const reviewId = createdNewReview.reviewId
-            console.log("reviewId: ", reviewId)
-            console.log("fileName: ", fileName)
+
             const createdReviewImage = await ReviewImage.create(reviewId, "/uploads/reviewImages/" + fileName);
-            console.log("createdReviewImage: ", createdReviewImage)
+
             if(!createdReviewImage) throw new Error("DB에 이미지 생성 실패");
     
             fs.rename(oldPath, newPath, async (err) => {
@@ -49,14 +44,13 @@ const reviewService = {
         
         createdNewReview.errorMessage = null;
 
-        console.log("location: ",createdNewReview)
         return createdNewReview;
     },
 
     getReviews: async ()=> {
 
         const reviews = await Review.findAll();
-        console.log("service:" ,reviews)
+
         return reviews;
     },
 
@@ -71,16 +65,11 @@ const reviewService = {
     setReview: async (reviewId, fields, files)=>{
         let review = null;
         const {title, contents, locationName, roadAddress} = fields
-        console.log("reviewId", reviewId)
-        console.log("reviewService: ", reviewId )
-        console.log("reviewService: ", fields )
-        console.log("reviewService: ", files )
 
         if (title) {
             const fieldToUpdate = "title";
             const newValue = title;
             review = await Review.update({ reviewId, fieldToUpdate, newValue });
-            console.log(review)
          }
 
         if (contents) {
@@ -140,10 +129,7 @@ const reviewService = {
                 const updatedReviewImage = await ReviewImage.update(reviewId, fieldToUpdate, newValue);
                 
                 review.reviewImage = updatedReviewImage.image;
-                // console.log("updatedReview: ",review)
                 fs.unlink(`src/uploads/reviewImages/${currentReviewImageInfo.image}`, (err) => {
-                    // if(err) throw new Error("이미지 삭제 실패");
-                    console.log("이미지 삭제 실패")
                 })
     
                 fs.rename(oldPath, newPath, (err) => {
@@ -166,8 +152,7 @@ const reviewService = {
             
             if(deletedReviewImage) {
                 fs.unlink(`src/uploads/images/${deletedReviewImage.image}`, (err) => {
-                    // if(err) throw new Error("이미지 삭제 실패");
-                    console.log("이미지 삭제 실패")
+
                 });
             } else {
                 return deletedReviewImage;
