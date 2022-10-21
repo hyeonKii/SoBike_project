026358d3081e5582f38119e6b30f86eafe4b56c3
[AI5/2반] 배성현 <lydiabae0719@gmail.com>
2022-10-21 13:19:06 +0000

    Update bicycleLocationRouter.js

--- back/src/routes/bicycleLocationRouter.js
+++ back/src/routes/bicycleLocationRouter.js
@@ -1,12 +1,7 @@
 import {Router} from "express";
-import { loginRequired } from "../middlewares/loginRequired";
 import {bicycleLocationService} from "../services/bicycleLocationService";
 
 const bicycleLocationRouter = Router();
-
-
-// a
-
 
 
 bicycleLocationRouter.get("/bicycle/location", async (req, res, next)=> {
@@ -23,8 +18,6 @@ bicycleLocationRouter.get("/bicycle/locationName", async (req, res, next)=> {
     try{
 
         const locationName = req.body.locationName
-
-        console.log(locationName)
         const location = await bicycleLocationService .getByLocationName({locationName});
         res.status(200).send(location);
     }catch(error){
@@ -35,7 +28,6 @@ bicycleLocationRouter.get("/bicycleAddress", async (req, res, next)=> {
     try{
         const locationName = req.body.locationName
 
-        console.log(locationName)
         const location = await bicycleLocationService .getAddressByLocationName({locationName});
         res.status(200).send(location);
     }catch(error){
@@ -52,13 +44,9 @@ bicycleLocationRouter.get("/bicycle/locations", async (req, res, next)=> {
 });
 bicycleLocationRouter.get("/bicycle/locationsByCurrentLocation?:userId?:latitude?:longitude", async (req, res, next)=> {
     try{
-        console.log("asfdsf")
         const userId = req.query.userId
-        console.log("userId: ",userId)
         const longitude = parseFloat(req.query.longitude);
         const latitude = parseFloat(req.query.latitude);
-        console.log("longitude: ", longitude)
-        console.log("latitude: ", latitude)
         const location = await bicycleLocationService.getLocationsByCurrentLocations({userId, longitude, latitude})
         res.status(200).send(location);
     }catch(error){
